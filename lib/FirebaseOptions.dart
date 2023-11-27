61049c538c8fdc6f04ad0a1d7970b5304ec9bd51
seanw <SeanCBrachvogel@gmail.com>
2023-11-27 13:21:47 -0500

    Added Map functionality
    (token is placeholdered since repo is public)

--- lib/FirebaseOptions.dart
+++ lib/FirebaseOptions.dart
@@ -64,5 +64,5 @@ class DefaultFirebaseOptions {
 //   storageBucket: '',
 //   iosClientId: '',
 //   iosBundleId: '',
- //);
-//}
+ );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -120,6 +121,46 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+//Create a map centered on Sydney, Australia.
+class OpenMap extends StatelessWidget {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-33.86, 151.20);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Maps Sample App'),
+            backgroundColor: Colors.green[700],
+          ),
+          body: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+            markers: {
+              const Marker(
+                markerId: MarkerId('Sydney'),
+                position: LatLng(-33.86, 151.20),
+                infoWindow: InfoWindow(
+                  title: "Sydney",
+                  snippet: "Capital of New South Wales",
+                ),
+              )
+            },
+          )
+      ),
     );
   }
 }
